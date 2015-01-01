a188fd52ca4874fb3856862987a19020338c61f3
MrDoel <l0c4l_r00t@yahoo.co.id>
2015-01-02 00:47:11 +0700

    Update RTSP-Scanner.sh

--- RTSP-Scanner.sh
+++ RTSP-Scanner.sh
@@ -2,7 +2,7 @@
 
 ####################################
 # RTSP SCANNER                     #
-#      							   #
+#                                  #
 # Coded by Mr.Doel                 #
 # Contact me : doel@mc-crew.or.id  #
 ####################################
@@ -46,33 +46,56 @@ function head {
 #Baca Network ID
 head
 echo -e "${rd}Press ctrl+c to exit${fin}"
+echo
 echo -en "${lcyn}Scanning Name ~>${fin} "
 read COMPANY
-echo -en "{lcyn}Input Network ID (1) ~>${fin} "
+echo -e "${lgrn}Set Company -> $COMPANY${fin}"
+
+echo && echo -en "${lcyn}Input Network ID (1) ~>${fin} "
 read NID1
-echo -en "{lcyn}Input Network ID (2) ~>${fin} "
+echo -e "${lgrn}Set Network ID (1) -> $NID1.xxx.xxx.xxx${fin}"
+
+echo && echo -en "${lcyn}Input Network ID (2) ~>${fin} "
 read NID2
-echo -en "{lcyn}Prefix ~> "
-read PREFIX
-echo -en "{lcyn}Input Range Awal ~>${fin} "
+echo -e "${lgrn}Set Network ID (2) -> $NID1.$NID2.xxx.xxx${fin}"
+
+echo && echo -en "${lcyn}Input Range Awal ~>${fin} "
 read BATAS_AWAL
-echo -en "{lcyn}Input Range Akhir ~> ${fin}"
+echo -e "${lgrn}Set Batas Awal -> $NID1.$NID2.$BATAS_AWAL.1/$PREFIX${fin}"
+
+echo && echo -en "${lcyn}Input Range Akhir ~> ${fin}"
 read BATAS_AKHIR
+echo -e "${lgrn}Set Batas Akhir -> $NID1.$NID2.$BATAS_AKHIR.1/$PREFIX${fin}"
+
+echo && echo -en "${lcyn}Prefix ~> "
+read PREFIX
+echo -e "${lgrn}Set Prefix -> /$PREFIX${fin}"
 
 let "BATAS_AWAL-=1"
 
+function inti {
 while : ; do
 	let BATAS_AWAL=BATAS_AWAL+1
+	
+	if [ -x "/usr/local/bin/tomb" ]; then
+	echo -e "Report will be save to ${grn}/opt/hasil_scanning${fin}"
+	else
+	sudo mkdir /opt/hasil_scanning
+	sudo chown $USER:$USER /opt/hasil_scanning
+	fi
+	cd /opt/hasil_scanning
 	sudo nmap -p554,80 --script http-title --open $NID1.$NID2.$BATAS_AWAL.1/$PREFIX -oX $COMPANY$BATAS_AWAL.xml -T4 -v
 	sudo xsltproc $COMPANY$BATAS_AWAL.xml -o $COMPANY$BATAS_AWAL.html
 	sudo rm $COMPANY$BATAS_AWAL.xml
-	sudo mkdir hasil_scanning
-	sudo mv $COMPANY$BATAS_AWAL.html hasil_scanning
 	if [ $BATAS_AWAL -eq $BATAS_AKHIR ]
 		then
 			break
 	fi
-done
+done	
+}
+
+inti
+
 
 
 
